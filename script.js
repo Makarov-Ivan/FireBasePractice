0e45a82f291ffd897fae75fd7e0f20fa4b154fa4
Makarov-Ivan <makivanik@gmail.com>
2020-04-25 01:17:25 +0300

    now we can fetch data from firestore

---
+++ script.js
@@ -0,0 +1,28 @@
+const cafeList = document.querySelector("#cafe-list"); //reference to list of cafes
+
+//create elements and render cafe
+function renderCafe(doc) {
+  let li = document.createElement("li");
+  let name = document.createElement("span");
+  let city = document.createElement("span");
+
+  li.setAttribute("data-id", doc.id); //adding data-id to li
+  name.textContent = doc.data().name; //determine text of name span
+  city.textContent = doc.data().city; //the same for city span
+
+  //nesting elements
+
+  li.appendChild(name);
+  li.appendChild(city);
+
+  cafeList.appendChild(li);
+}
+
+db.collection("cafes")
+  .get() //this grab all documents form CAFES collection in our DataBase
+  .then((snapshot) => {
+    //.get is an asynchronous function, so we use then. parameter is an spashot of our DataBase(array). for getting an data form DB, we should use .data() method for elements in this snapshot
+    snapshot.docs.forEach((doc) => {
+      renderCafe(doc);
+    });
+  });
